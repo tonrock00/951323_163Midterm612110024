90b7082f066c8676e827650674d893c3326e03c0
DESKTOP-325D3AA\612110024 <thitipong_janjom@cmu.acth>
2020-10-03 11:28:31 +0700

    Update 11.28 AM

---
+++ Assets/Script/MainMenuControlScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MainMenuControlScript : MonoBehaviour
+{
+    [SerializeField] Button _stageselectButton;
+    [SerializeField] Button _optionsButton;
+    [SerializeField] Button _exitButton;
+    [SerializeField] Button _creditsButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _stageselectButton.onClick.AddListener(delegate { StageSelectButtonClick(_stageselectButton); });
+        _optionsButton.onClick.AddListener(delegate { OptionsButtonClick(_optionsButton); });
+        _exitButton.onClick.AddListener(delegate { ExitButtonClick(_exitButton); });
+        _creditsButton.onClick.AddListener(delegate { CreditsButtonClick(_creditsButton); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StageSelectButtonClick(Button button)
+    {
+        if (!GameApplicationManager.Instance.IsStagesSelectActive)
+        {
+            SceneManager.LoadScene("SceneStages", LoadSceneMode.Additive);
+            GameApplicationManager.Instance.IsStagesSelectActive = true;
+        }
+    }
+    public void OptionsButtonClick(Button button)
+    {
+        if (!GameApplicationManager.Instance.IsOptionMenuActive)
+        {
+            SceneManager.LoadScene("SceneOptions", LoadSceneMode.Additive);
+            GameApplicationManager.Instance.IsOptionMenuActive = true;
+        }
+    }
+    public void ExitButtonClick(Button button)
+    {
+        Application.Quit();
+    }
+    public void CreditsButtonClick(Button button)
+    {
+        SceneManager.LoadScene("SceneCredits");
+    }
+}
---
+++ Assets/Script/StagesSelectionControlScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StagesSelectionControlScript : MonoBehaviour
+{
+    [SerializeField] Button _stage1Button;
+    [SerializeField] Button _stage2Button;
+    [SerializeField] Button _backButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _stage1Button.onClick.AddListener(delegate { Stage1ButtonClick(_stage1Button); });
+        _stage2Button.onClick.AddListener(delegate { Stage2ButtonClick(_stage2Button); });
+        _backButton.onClick.AddListener(delegate { BackButtonClick(_backButton); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BackButtonClick(Button button)
+    {
+        SceneManager.UnloadSceneAsync("SceneStages");
+        GameApplicationManager.Instance.IsStagesSelectActive = false;
+    }
+    public void Stage1ButtonClick(Button button)
+    {
+        SceneManager.UnloadSceneAsync("SceneStages");
+        SceneManager.LoadScene("SceneMainMenu");
+    }
+    public void Stage2ButtonClick(Button button)
+    {
+        SceneManager.UnloadSceneAsync("SceneStages");
+        SceneManager.LoadScene("SceneMainMenu");
+    }
+}
